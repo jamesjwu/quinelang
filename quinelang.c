ce92ad073a04b54928f41e505e583582df9c82b6
James Wu <jameswu@cmu.edu>
2014-03-06 22:09:38 -0500

    Commented source code

--- quinelang.c
+++ quinelang.c
@@ -1,19 +1,24 @@
 #include <stdlib.h>
 #include <stdio.h>
 
-int main(int argc, char **argv) {
+int main(int argc, char **argv) 
+{
+	//Read in a file!
 	if(argc != 2) {
 		fprintf(stderr, "Usage: qlc filename.qlc\n");
 		exit(1);
 	}
+
+	//Run the file using sophisticated compilation algorithm
 	int c;
 	FILE *file;
 	file = fopen(argv[1], "r");
 	if (file) {
+		//much algorithm
 	    while ((c = getc(file)) != EOF)
 	        putchar(c);
 	    printf("\n");
 	    fclose(file);
 	}
 	return 0;
-	}
+}
